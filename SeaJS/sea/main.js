ea90af444938ed037f99a2e99bb1efe9da358abe
HHT <956184070@qq.com>
2016-11-08 15:14:25 +0800

    SeaJS

--- SeaJS/sea/main.js
+++ SeaJS/sea/main.js
@@ -2,10 +2,22 @@ define(function(require,exports,module){
 
 	var math=require("math");
 	var common=require("common");
+	//调用jquery包
+	var $=require("jquery_sea");
 
-	exports.fun=function(a,b){
+	exports.fun=function(){
 
-		common.tips(math.sum(a,b));
+		$(".add").click(function(){
+
+			var a=parseFloat($("#value1").val());
+			var b=parseFloat($("#value2").val());
+			var sum=0;
+			common.log((sum=math.sum(a,b)));
+			$("#sum").html(sum);
+
+		});
 	}
+
+
 
 });
