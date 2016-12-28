c4e47f5bb0c14c8ef5d9a6a40e339c69ee6b93eb
Hunter <956184070@qq.com>
2016-12-28 16:27:42 +0800

    设计模式

--- JavaScript设计模式与实践/1.对象的多态性.js
+++ JavaScript设计模式与实践/1.对象的多态性.js
@@ -1,1 +1,24 @@
-var funciton(){}
+function makeSound(animal){
+	animal.sound();
+}
+
+//鸭子对象
+function Duck(){}
+
+Dock.prototype.sound=function(){
+	console.log("嘎嘎！");
+}
+
+//小狗对象
+function Dog(){}
+
+Dog.prototype=function(){
+	console.log("汪汪！");
+}
+
+
+//小猫对象
+function Cat(){}
+Cat.prototype.sound=function(){
+	console.log("喵喵！");
+}
